03a1102d8efeb5671b01ce6c1b2762bb6cfafc7b
Tantō Defi <107724252+tantodefi@users.noreply.github.com>
2025-02-01 11:02:02 -0600

    fix metadata

---
+++ app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'LUKSO Tip Gate MiniApp Nextjs Template',
+  description: 'Add a tip gate to any content on LUKSO',
+}; 
